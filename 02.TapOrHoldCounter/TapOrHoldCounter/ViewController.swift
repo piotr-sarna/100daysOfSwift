bf6858294a11e9eaadd279b7a05ee91828821e2c
Piotr Sarna <piotr@sarna.net.pl>
2018-04-12 19:51:58 +0200

    Clone 01.TapCounter project and rename it

---
+++ 02.TapOrHoldCounter/TapOrHoldCounter/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  TapOrHoldCounter
+//
+//  Created by Piotr Sarna on 10.04.2018.
+//  Copyright © 2018 Piotr Sarna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet private weak var counterLabel: UILabel!
+    private var counter: Int = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        counter = 0
+        updateCounterLabel()
+    }
+    
+    private func updateCounterLabel() {
+        let formatter = createNumberFormatter()
+        let formattedNumber = formatter.string(from: counter as NSNumber)
+        
+        counterLabel.text = formattedNumber
+    }
+    
+    private func createNumberFormatter() -> NumberFormatter {
+        let formatter = NumberFormatter()
+        
+        formatter.numberStyle = .none
+        
+        return formatter
+    }
+
+    @IBAction private func tapButtonTouchUpInside(_ sender: UIButton) {
+        counter += 1
+        updateCounterLabel()
+    }
+    
+    @IBAction private func resetButtonAction(_ sender: UIBarButtonItem) {
+        counter = 0
+        updateCounterLabel()
+    }
+    
+}
+
